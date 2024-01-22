6d17a74f615a9fe514d3b50f7b041c0e2043560f
ValleirMalta <31601403+ValleirMalta@users.noreply.github.com>
2024-01-22 17:12:00 -0300

    EXPERIENCE STARTED

---
+++ components/AboutMe.tsx
@@ -0,0 +1,25 @@
+import styles from '@/styles/Home.module.scss';
+export default function AboutMe(){
+    return (
+        <>
+            <section className={styles.aboutMe}>
+              <div>
+                <h3 className={styles.tituloSection}>SOBRE MIM</h3>
+                <p>
+                  Gosto de desafios, trabalho bem em equipe e aprendo bastante as coisas
+                  sozinho. Sou autodidata.
+                  Sou um desenvolvedor front-end com experiência na área e estou
+                  desenvolvendo minhas habilidades para back-end. Trabalho com
+                  frameworks que facilitam na agilidade do desenvolvimento do projeto.
+                </p>
+              </div>
+              <div className={styles.personalInfor}>
+                  <p>Brazil, 31.03.1995</p>
+                  <p>Curitiba, Paraná, Brasil</p>
+                  <p><strong>+55 (41) 984016243</strong></p>
+                  <p><strong>valleir.sud@gmail.com</strong></p>
+              </div>
+            </section>
+        </>
+    )
+}
