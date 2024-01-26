9f829446fa2978c04f0fd478d4c9a7afe9d53218
ValleirMalta <31601403+ValleirMalta@users.noreply.github.com>
2024-01-26 19:29:42 -0300

    New and changing fonts have been added to the page

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -10,7 +10,6 @@ interface listExperience {
     responsibility: string,
 }
 export default function Experience(){
-    const [item, setItem] = useState(0);
     const experienceList: listExperience[] = [
         {
             startDate: "Março 2014",
@@ -27,55 +26,47 @@ export default function Experience(){
             vacancyInfor: "Trabalhei na produção de arte para panfletos de produtos, criando peças visuais que ajudaram a promover e destacar as características e benefícios dos produtos para os clientes."
         },
         {
-            startDate: "Março 2015",
-            endDate: "Julho 2015",
-            companyName: "Gonçalves IND.",
-            responsibility: "Designer",
-            vacancyInfor: "Trabalhei na produção de arte para panfletos de produtos, criando peças visuais que ajudaram a promover e destacar as características e benefícios dos produtos para os clientes."
+            startDate: "Fevereiro 2016",
+            endDate: "Novembro 2017",
+            companyName: "Agência BPSI",
+            responsibility: "Automação de Marketing , Arte Finalista, Desenvolvedor Web.",
+            vacancyInfor: "Trabalhei na produção de arte para campanhas no Facebook, na automação de marketing utilizando a plataforma RD Station, e também como desenvolvedor front-end em um site de doações."
         },
         {
-            startDate: "Março 2015",
-            endDate: "Julho 2015",
-            companyName: "Gonçalves IND.",
-            responsibility: "Designer",
-            vacancyInfor: "Trabalhei na produção de arte para panfletos de produtos, criando peças visuais que ajudaram a promover e destacar as características e benefícios dos produtos para os clientes."
+            startDate: "Fevereiro 2018",
+            endDate: "Fevereiro 2021",
+            companyName: "Hospital Central de Porto Velho",
+            responsibility: "Auxiliar de farmácia hospitalar.",
+            vacancyInfor: "Trabalhei na organização e distribuição de medicação para todos os setores do hospital."
+        },
+        {
+            startDate: "Outubro 2020",
+            endDate: "Janeiro 2021",
+            companyName: "Soma Comunicação e Eventos",
+            responsibility: "Arte Finalista.",
+            vacancyInfor: "Trabalhei na produção de arte para campanhas publicitárias no Facebook."
+        },
+        {
+            startDate: "Abril de 2021",
+            endDate: "Outubro 2023",
+            companyName: "Domatech Soluções em tecnologia",
+            responsibility: "Desenvolvedor Front-End.",
+            vacancyInfor: "Trabalho com a criação de páginas front-end utilizando a tecnologia ReactJS e TypeScript."
         }
     ]
     function renderExperience () {
         return experienceList.map((experience, i) => 
         <li key={i} className={styles.item}>
-            <div className={styles.headerItem}>
-                <button
-                className={` ${styles.btnArrow} ${item === (i + 1) ? styles.btnArrowExpanded : ""} `}
-                onClick={() => expandItem(i)}
-                >
-                    <Image
-                        src={arrowSvg}
-                        width={30}
-                        height={30}
-                        alt="Picture of the author"
-                    />
-                </button>
-                <span className={styles.itemDot}></span>
-                <div>
-                    <span className={styles.itemDate}>{experience.startDate} - {experience.endDate}</span>
-                    <p className={styles.itemName}><strong>{experience.companyName}</strong><span> — {experience.responsibility}</span></p>
-                </div>
-            </div>
-            <div className={` ${styles.itemInfor} ${item === (i + 1) ? styles.expanded : ""} `} >
-                <div>
+            <span className={styles.itemDot}></span>
+            <div>
+                <span className={styles.itemDate}>{experience.startDate} - {experience.endDate}</span>
+                <p className={styles.itemName}><strong>{experience.companyName}</strong><span> — {experience.responsibility}</span></p>
+                <div className={styles.itemInfor}>
                     <p>{experience.vacancyInfor }</p>
                 </div>
             </div>
         </li>
         )
-    }
-    function expandItem(params:number) {
-        if((params + 1) === item) {
-            setItem(0)
-        } else {
-            setItem(params + 1)
-        }
     }
 
     return (
