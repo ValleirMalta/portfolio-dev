2594e835cc233d019e497fb3574168fcf5ae286a
ValleirMalta <31601403+ValleirMalta@users.noreply.github.com>
2024-02-17 00:59:42 -0300

    Criação de componente de select

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,24 @@
+
+import React, { useRef } from 'react';
 import HeadInfor from '@/components/HeadInfor';
 import AboutMe from '@/components/AboutMe';
 import Header from '@/components/Header';
 import Skills from '@/components/Skills';
 import Experience from '@/components/Experience';
+import SelectOne from '@/components/Select/SelectOne';
 import styles from '@/styles/Home.module.scss';
 
 export default function Home() {
+  const checkboxRef = useRef<HTMLLabelElement>(null);
   return (
     <>
       <HeadInfor
         title='Front-End: Valleir Malta'
         description='Curriculo Online'
       />
-      <main className="mainHome">
-        <div className="containerHome">
+      <main className="mainHome"  ref={checkboxRef}>
+        <SelectOne checkboxRef={checkboxRef} />
+        <div className="container">
           <Header />
           <AboutMe />
           <Skills />
@@ -23,3 +28,4 @@ export default function Home() {
     </>
   )
 }
+
